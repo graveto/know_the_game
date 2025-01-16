dd1e248b3067e9a7a7422e9517c1efec58cedc50
David Fuka <gravetotkd@gmail.com>
2025-01-15 21:05:56 -0700

    Refactored background color

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:not_my_first_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 245, 236, 17),
+            Color.fromARGB(255, 255, 193, 67)
+          ])),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
