f0e9054393545301deae0447fdd07319daa92c42
David Fuka <gravetotkd@gmail.com>
2025-01-15 20:56:09 -0700

    Initial commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 245, 236, 17),
+            Color.fromARGB(255, 255, 193, 67)
+          ])),
+      child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 300,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Learn Halo: Flashpoint the Fun Way!',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              OutlinedButton(
+                onPressed: null,
+                style: OutlinedButton.styleFrom().copyWith(
+                  side: WidgetStateProperty.all(
+                    const BorderSide(color: Colors.white),
+                  ),
+                ),
+                child: const Text(
+                  'Start Quiz',
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
